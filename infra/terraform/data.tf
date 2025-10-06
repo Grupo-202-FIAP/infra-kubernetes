b337b7002343b3b5fa2b83dfaa8fd7580b96a179
leobr1t0 <leonardo.oliveira@sptech.school>
2025-10-06 19:49:01 -0300

    ajuste formatacao

--- infra/terraform/data.tf
+++ infra/terraform/data.tf
@@ -1,8 +1,8 @@
-  data "terraform_remote_state" "network" {
-    backend = "s3"
-    config = {
-      bucket = "terraform-state-bucket-nextime"
-      key    = "infra.tfstate"
-      region = "us-east-1"
-    }
+data "terraform_remote_state" "network" {
+  backend = "s3"
+  config = {
+    bucket = "terraform-state-bucket-nextime"
+    key    = "infra.tfstate"
+    region = "us-east-1"
   }
+}
